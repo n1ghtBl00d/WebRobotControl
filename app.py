8292b036c7d921febe5f029481f21ac452b2d1cd
n1ghtBl00d <zachlutah@gmail.com>
2022-04-07 03:14:22 +0100

    First iteration of Webcam FPV Uses too much memory on my RasPi 2B, and crashes

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from crypt import methods
 from urllib import response
-from flask import Flask, render_template, request, redirect, url_for, make_response
+from flask import Flask, render_template, request, redirect, url_for, make_response, Response
 from flask_socketio import SocketIO
 #from RobotRC import RobotRC as RobotControl
 #from RobotTank import RobotTank as RobotControl
 from RobotArcade import RobotArcade as RobotControl
+import cv2
 
 RobotControl.setup()
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+camera = cv2.VideoCapture(-1)
+enableCamera = 1
 
 @app.route("/")
 def index():
@@ -47,5 +51,33 @@ def handleStop(data):
 
 @socketio.on('robotControl')
 def handleControl(data):
-    print(data)
+    #print(data)
     RobotControl.update(data['axisL'], data['axisR'])
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(genFrame(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def genFrame():
+    global enableCamera
+    while enableCamera:
+        print("Camera")
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+
+            scale = 50
+            width = int(frame.shape[1] * scale / 100)
+            height = int(frame.shape[0] * scale / 100)
+            dim = (width, height)
+
+            resized = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+
+            ret, buffer = cv2.imencode('.jpg', resized)
+            newFrame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + newFrame + b'\r\n')
